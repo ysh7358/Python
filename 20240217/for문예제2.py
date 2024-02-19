79b869cb026c0e306d281cbc10df52ebb77f4d2f
ysh7358 <yeosh7358@gmail.com>
2024-02-19 17:00:12 +0900

    20240219

--- 20240217/for문예제2.py
+++ 20240217/for문예제2.py
@@ -17,9 +17,107 @@ for c in range(1, 11) :
 for d in range (10, 0, -1) :
     if d%2 == 0 :
         print(d)
-'''
+
+#1부터 10까지의 합
 sum = 0
 
 for e in range (1, 11) :
     sum = sum + e
     print("1부터" , e, "까지의 합 : ", sum)
+
+#1부터 입력한 값까지 홀/짝수의 합
+
+print("1부터 입력한 수 까지의 홀수 짝수의 합을 구하는 프로그램")
+
+sum1 = 0
+sum2 = 0
+
+e = int(input("숫자를 입력해 주세요 : "))
+
+for e in range (e+1) :
+    if e % 2 == 1 :
+        sum1 = sum1 + e
+        
+    elif e % 2 == 0 :
+        sum2 = sum2 + e
+        
+print("1부터" , e, "까지의 홀수 합 : ", sum1)
+print("1부터" , e, "까지의 짝수 합 : ", sum2)
+
+#1부터 입력한 값까지 홀/짝수의 곱
+
+print("1부터 입력한 수 까지의 홀수 짝수의 합을 구하는 프로그램")
+
+num1 = 1
+num2 = 1
+
+e = int(input("숫자를 입력해 주세요 : "))
+
+for e in range (1, e+1) :
+    if e % 2 == 1 :
+        num1 = num1 * e
+        
+    elif e % 2 == 0 :
+        num2 = num2 * e
+        
+print("1부터" , e, "까지의 홀수 곱 : ", num1)
+print("1부터" , e, "까지의 짝수 곱 : ", num2)
+
+print("1부터 입력한 수 까지의 전체와 홀짝으로 덧셈 곱셈을 하는 프로그램")
+print()
+
+sum1 = 0
+sum2 = 0
+num1 = 1
+num2 = 1
+
+f = int(input("숫자를 입력해 주세요 : "))
+print("숫자", f ,"를 입력하셨습니다")
+print()
+
+for e in range (1, f+1) :
+    
+    if e % 2 == 1 :
+        sum1 = sum1 + e
+        num1 = num1 * e
+        
+    elif e % 2 == 0 :
+        num2 = num2 * e
+        sum2 = sum2 + 2
+        
+sum = sum1 + sum2
+num = num1 * num2
+
+print("1부터" , f, "까지의 홀수 합 : ", sum1)
+print("1부터" , f, "까지의 홀수 곱 : ", num1)
+print("1부터" , f, "까지의 짝수 합 : ", sum2)
+print("1부터" , f, "까지의 짝수 곱 : ", num2)
+print("1부터" , f, "까지의 합 : ", sum)
+print("1부터" , f, "까지의 곱 : ", num)
+'''
+#농구선수 10명의 키를 입력받아서 키의 총합을 출력
+sum = 0
+maxheight = 0
+minheight = 0
+index1 = 0
+index2 = 0
+
+for i in range(10) :
+    height = int(input("농구선수의 키를 입력해주세요 (단위 : cm) : "))
+    if i == 0 :
+        minheight = height
+        
+    else : 
+        if height > maxheight :
+           maxheight = height
+           index1 = i+1
+           
+        if height < minheight :
+            minheight = height
+            index2 = i+1
+            
+    sum = sum + height
+    
+print("농구선수의 키의 총합은 ",sum,"cm 입니다")
+print("가장 작은 키를 가진 선수는 ", index2, "번째 선수로," ,minheight, "cm 입니다")
+print("가장 큰 키를 가진 선수는 ", index1, "번째 선수로," ,maxheight, "cm 입니다")
--- 20240217/동전게임.py
+++ 20240217/동전게임.py
@@ -17,16 +17,20 @@ if player == computer :
 else :
     print("안빙고")
 '''
-
+winCount = 0
 s = ['앞' , '뒤' ]
 
-computer = random.choice(s)
-print(computer)
-player = input("앞 / 뒤! 어디에 거시겠습니까 : ")
+for i in range(10) :
+    print(str(i+1) + "번째 게임입니다.")
+    computer = random.choice(s)
+    print(computer)
+    player = input("앞 / 뒤! 어디에 거시겠습니까 : ")
 
-if player == computer :
-    print("빙고")
+    if player == computer :
+        print("빙고\n")
+        winCount = winCount + 1
 
-else :
-    print("안빙고")
-    
+    else :
+        print("안빙고\n")
+
+print("승리한 횟수는 " + str(winCount)+ "회입니다.")
